90c871e7929365913a453f1e0f1d70d21a68ce67
Nikhil Dvivedi <ndvivedi@gmail.com>
2013-08-15 20:45:00 -0700

    fixed broken impl

--- src/main/scala/boggle/Trie.scala
+++ src/main/scala/boggle/Trie.scala
@@ -1,28 +1,26 @@
 object Trie {
   import scala.collection._
 
-  def apply[A](items: List[List[A]]) = 
+  def apply[A <% Ordered[A]](items: List[List[A]]) = 
     items.foldRight(empty[A])((i:List[A],t:Trie[A]) => t.addItem(i))
-  def empty[A] = Trie(immutable.Map[A, Trie[A]](), false)
+  def empty[A <% Ordered[A]] = Trie(immutable.TreeMap[A, Trie[A]](), false)
 
-  case class Trie[A](next: immutable.Map[A, Trie[A]], end: Boolean) {
+  case class Trie[A <% Ordered[A]](next: immutable.TreeMap[A, Trie[A]], end: Boolean) {
     def addItem(item: List[A]): Trie[A] = item match {
       case Nil => Trie[A](next, true)
-      case h::t => Trie(next + (h -> next.getOrElse(h, empty).addItem(t)), end)
+      case h::t => Trie[A](next + (h -> next.getOrElse(h, empty[A]).addItem(t)), end)
     }
 
-
     def full(item: List[A]): Boolean = 
-      item match {
-        case c::cs => next(c).full(cs)
-        case Nil => end
-      }
+      node(item).map {x => x.end} getOrElse false
 
     def prefix(item: List[A]): Boolean =
-      item match {
-        case Nil => true
-        case h::t => next(h).prefix(t)
-      }
+      node(item).map {x => true} getOrElse false
+
+    def node(as: List[A]): Option[Trie[A]] = as match {
+      case Nil => Some(this)
+      case h::t => next.get(h).flatMap(_.node(t))
+    }
   }
 }
 
